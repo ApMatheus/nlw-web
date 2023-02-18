94f0d631e4a626495b0f6a0fa1ccb75aacb86e11
ApMatheus <matheusaparecido101@gmal.com>
2023-02-18 11:02:24 -0300

    first commit

---
+++ src/Components/ProgessBar.tsx
@@ -0,0 +1,21 @@
+
+interface ProgessBar{
+    progress: number
+}
+
+export function ProgressBar(props: ProgessBar) {
+    return (
+        <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+            <div
+                role="progressbar"
+                aria-label="progresso de habitos completados nesse dia "
+                aria-valuenow={props.progress}
+                className="h-3 rounded-xl bg-violet-600 transition-all"
+                style={{
+                    width:`${props.progress}%`
+                }} 
+            />
+        </div>
+
+    )
+}
